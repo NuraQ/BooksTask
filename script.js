a8f6191211583732098983d060d7f83cb3fc8c4a
NuraQ <nura.qasrawi@gmail.com>
2021-02-22 11:19:37 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,92 @@
+var currentIndex = 0;
+var totalItemsCount = 0;
+var search = false;
+window.onscroll = function () {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    if (totalItemsCount - currentIndex >= 40) {
+        console.log(currentIndex,"curr")
+      currentIndex += 40;
+    } else {
+      //currentIndex += totalItemsCount - currentIndex;
+    }
+    if (!search) {
+      fetchData();
+    } else {
+        getSearchApiResult();
+    }
+  }
+};
+function fetchData() {
+    console.log(currentIndex,"curr in fetch")
+
+  fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=intitle:&maxResults=40&start-index=${currentIndex}&key=AIzaSyCOBbymaad4eBVNFVF5JC-Pc0TQzE6AHOw`,
+    {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      totalItemsCount = data.totalItems;
+      displayBooks(data.items);
+    });
+}
+
+function displayBooks(data) {
+  var div = document.getElementById("content");
+  for (item of data) {
+    col = document.createElement("div");
+    img = document.createElement("img");
+    col.className = "column";
+    p = document.createElement("p");
+    text = document.createTextNode(`${item.volumeInfo.title}`);
+    img.src = item.volumeInfo.imageLinks
+      ? item.volumeInfo.imageLinks.thumbnail
+      : "";
+    img.className = "center";
+    img.loading = "lazy";
+    p.appendChild(text);
+    col.appendChild(img);
+    col.appendChild(p);
+    div.appendChild(col);
+  }
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function filter() {
+  currentIndex = 0;
+  search = true;
+  console.log(input, "in");
+  const container = document.querySelector("#content");
+  removeAllChildNodes(container);
+  getSearchApiResult();
+}
+
+function getSearchApiResult() {
+  var input = document.getElementById("search").value;
+  fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=intitle:${input}&maxResults=40&start-index=${currentIndex}&key=AIzaSyCOBbymaad4eBVNFVF5JC-Pc0TQzE6AHOw`,
+    {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data, "result");
+      totalItemsCount = data.totalItems;
+
+      displayBooks(data.items);
+    });
+}
