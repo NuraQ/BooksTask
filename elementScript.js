b5b39304193d004450fe665ead453dbbfc716f71
NuraQ <nura.qasrawi@gmail.com>
2021-02-23 10:09:18 +0200

    added subtitle

--- elementScript.js
+++ elementScript.js
@@ -1,13 +1,17 @@
-
-let bookObject = null
+let bookObject = null;
 window.onload = function () {
-    bookObject = localStorage.getItem('bookObj');
-    let book = JSON.parse(bookObject)
-    let container = document.getElementById("container")
-    let para = document.getElementById("info")
-    let image = document.getElementById("itemImage")
-    let details = document.getElementById("description")
-    para.innerHTML += book.volumeInfo.title   
-    image.src = book.volumeInfo.imageLinks.thumbnail
-    details.innerHTML = book.volumeInfo.description
- };
+  bookObject = localStorage.getItem("bookObj");
+  let book = JSON.parse(bookObject);
+  let container = document.getElementById("container");
+  let para = document.getElementById("info");
+  let image = document.getElementById("itemImage");
+  let details = document.getElementById("description");
+  
+  para.innerHTML +=
+    book.volumeInfo.title +
+    `<br/><br/>` +
+    (book.volumeInfo.subtitle ? book.volumeInfo.subtitle : "");
+  console.log(book);
+  image.src = book.volumeInfo.imageLinks.thumbnail;
+  details.innerHTML = book.volumeInfo.description;
+};
